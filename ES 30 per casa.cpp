f75cbadbbb6e36c8a2d817ffa8d0f991dd1ace09
Andacar <carrer.renzo@libero.it>
2016-11-04 10:24:33 +0100

    Add files via upload

---
+++ ES 30 per casa.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+int numero;
+int centinaia=0;
+int decine=0;
+int unita=0;
+int main()
+{
+    cout << "inserisci un numero intero (inferiore a mille) ";
+    cin >> numero;
+
+    while (numero>100) {
+      numero=numero-100;
+      centinaia++;
+    }
+    while (numero>10) {
+        numero=numero-10;
+        decine++;
+    }
+    while (numero>0) {
+        numero=numero-1;
+        unita++;
+    }
+cout << "centinaia " <<centinaia << endl;
+cout << "decine " <<decine << endl;
+cout << "unita " << unita<< endl;
+
+
+}
